4b14fa1d6e8687779c90b869584cb759499f8cdd
Dave Fetterman <fettermania@gmail.com>
2022-02-17 19:58:24 -0800

    Fixing basic routing

--- routes.js
+++ routes.js
@@ -5,11 +5,8 @@ const routes = require('next-routes')();
 // TODONEW: get rid of all routes?  Just index page?
 // NOTE: This matches IN ORDER, so above shadows below.
 
-// routes
-// 	.add('/campaigns/new', '/campaigns/new') // Overrides default
-// 	.add('/campaigns/:address', '/campaigns/show')
-// 	.add('/campaigns/:address/requests', '/campaigns/requests/index')
-// 	.add('/campaigns/:address/requests/new', '/campaigns/requests/new');
+routes
+	.add('Individual Square', '/squares/:address', '/show');
 
 // NOTE: this is an OBJECT that generates helpers we use everywhere.
 module.exports = routes;
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const next = require('next')
 // TODONEW: Need to flip this for "production mode" next.js - what is this?
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 
-const routes = require('./routes')
+const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3001, (err) => {
+  createServer(handler).listen(3000, (err) => {
   	if (err) throw err;
-  	console.log("Ready on localhost:3001");
+  	console.log("Ready on localhost:3000");
   })
 })
