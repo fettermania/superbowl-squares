d6fb7c36bb0dbd90694204d1f516f39542b65658
Dave Fetterman <fettermania@gmail.com>
2024-02-07 14:09:32 -0800

    Remove broken stlye link (mirror part of main fb87a4e)

--- components/Layout.js
+++ components/Layout.js
@@ -16,10 +16,6 @@ import Header from './Header';
 	    	    rel="stylesheet"
   		        href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
   			  />
-  			    <link async
-	    	    rel="stylesheet"
-  		        href="/styles.css"
-  			  />
   			 </Head>
 
 
